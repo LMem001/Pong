282ba7ecc4cac04616819f7588fc61c7533e99d9
Luca Memeo <memeoluca6@gmail.com>
2024-08-01 21:46:25 +0200

    reset players
    created group to reset players

--- pong/scenes/player1.gd
+++ pong/scenes/player1.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 var paddle_half_height
+var initial_position = Vector2()
 
 func _physics_process(delta):
 	_move_direction()
@@ -8,8 +9,12 @@ func _physics_process(delta):
 	position.y = clamp(position.y, paddle_half_height, viewport_rect.size.y - paddle_half_height)
 
 func _ready():
+	initial_position = position
 	paddle_half_height = %Collision.shape.extents.y
 	
 func _move_direction():
 	var direction = Input.get_vector("", "", "left_moves_up", "left_moves_down")
 	velocity = direction * 600
+	
+func reset_player():
+	position = initial_position
--- pong/scenes/pong.gd
+++ pong/scenes/pong.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 func _on_goal_left_area_entered(area):
 	%Ball.reset_ball("right")
+	get_tree().call_group("Players", "reset_player")
 
 func _on_goal_right_area_entered(area):
 	%Ball.reset_ball("left")
+	get_tree().call_group("Players", "reset_player")
