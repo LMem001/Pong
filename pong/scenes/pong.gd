2c17a919e6db49a9ad03fb44cf7d85e50d504d03
Luca Memeo <memeoluca6@gmail.com>
2024-08-01 12:46:17 +0200

    added x_direction to the game
    The Ball will go in the direction of the player who won last time

--- pong/scenes/pong.gd
+++ pong/scenes/pong.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-
 func _on_goal_left_area_entered(area):
-	%Ball.reset_ball()
+	%Ball.reset_ball("right")
 
 func _on_goal_right_area_entered(area):
-	%Ball.reset_ball()
+	%Ball.reset_ball("left")
