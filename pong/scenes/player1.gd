55005f0124da46fd008085ff2c0ca19c7b0748c4
Luca Memeo <memeoluca6@gmail.com>
2024-08-10 23:42:29 +0200

    minor fixes

--- pong/scenes/player1.gd
+++ pong/scenes/player1.gd
@@ -1,16 +1,12 @@
 extends CharacterBody2D
-var paddle_half_height
 var initial_position = Vector2()
 
 func _physics_process(delta):
 	_move_direction()
 	move_and_slide()
-	var viewport_rect = get_viewport().get_visible_rect()
-	position.y = clamp(position.y, paddle_half_height, viewport_rect.size.y - paddle_half_height)
 
 func _ready():
 	initial_position = position
-	paddle_half_height = %Collision.shape.extents.y
 	
 func _move_direction():
 	var direction = Input.get_vector("", "", "left_moves_up", "left_moves_down")
